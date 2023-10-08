0faddd4036c4e4e0a48c723a39fe6dfded313375
SpooktoberFest <146979839+SpooktoberFest@users.noreply.github.com>
2023-10-08 18:50:31 +0200

    Added sorter & test

--- main.py
+++ main.py
@@ -1,41 +1,62 @@
-
+#======================================================================================
+#                                   Imports
+#======================================================================================
 import json
+from operator import attrgetter
 
-# Load data from JSON file
-def loadBoxesData(infilename):
+
+#======================================================================================
+#                                   Auxiliary functions
+#======================================================================================
+
+
+def loadBoxesData(infilename): # Load data from JSON file
     
     # Read from JSON file
     with open(infilename, "r") as infile:
         data = json.load(infile)
-    
     return data
 
-# Output data to JSON file
-def outputData(data, outfilename):
+
+def outputData(data, outfilename): # Output data to JSON file
 
     # Format data to JSON object
     json_object = json.dumps(data, indent=4)
-
     # Write to file
     with open(outfilename, "w") as outfile:
         outfile.write(json_object)
 
 
+def sortData(data): # Build new sorted list 
+    
+    # Sort after dest_id first and weight second (largest numbers first)
+    dataout = sorted(data, key=attrgetter('dest_id', 'weight'), reverse=True)
+    return dataout
 
-# Defining main function
+
+
+
+
+
+#======================================================================================
+#                                   Main function
+#======================================================================================
 def main():
 
     # Hyperparameters
     infilename = 'boxes.json'
     outfilename = 'out.json'
+    
 
     # Load data on boxes
     data = loadBoxesData(infilename)
 
 
+    # Sort boxes
+    data = sortData(data)
 
 
-    # Iterating through the json list
+    # Iterate through boxes
     for box in data:
         print(box)
 
@@ -45,12 +66,13 @@ def main():
     # Output palleting solution
     outputData(data, outfilename)
     
+
     return 0
-  
-  
-# Execute main function
+
+
+#======================================================================================
+#                                   Execute
+#======================================================================================
 if __name__=="__main__": 
     main() 
-
-
 
